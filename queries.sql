1cdbed6cd9e082a83a99dcc2fa26e212b4bebb89
echuung94 <you@example.com>
2021-01-21 19:00:24 -0500

    csv and queries.

---
+++ queries.sql
@@ -0,0 +1,24 @@
+-- employees born between 1952 and 1955, who were also hired between 1985 and 1988.
+SELECT first_name, last_name
+FROM employees 
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+
+SELECT first_name, last_name
+FROM employees 
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'; 
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--telling Postgres to save the data into a table named "retirement_info."
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
